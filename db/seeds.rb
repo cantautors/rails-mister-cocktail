5fdd7abdcbb6a510dd66978a6e07305cc681bdb0
Karolina Paulauskaite <carolina.paulauskaite@gmail.com>
2019-02-15 16:55:43 +0100

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "Lime")
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Rum")
+Ingredient.create(name: "Grapefruit")
+Ingredient.create(name: "Pineapple")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Aperol")
+Ingredient.create(name: "Prosecco")
+Ingredient.create(name: "Sparkling water")
+Ingredient.create(name: "Maple Syrup")
+Ingredient.create(name: "OJ")
+Ingredient.create(name: "Cranberry Juice")
+
+
